Mouvedia/cb-fetch

commit 7e56833a7404a8a89b686c53eb350bfdb63d9c7f

feat(XHR): support obsolete line folding and LF as a line terminator

index.js
@@ -390,7 +390,7 @@
                           'MSXML.DOMDocument'],
         len            = progIDs.length,
         queryLanguage  = cfg.settings && cfg.settings.XSLPattern ? 'XSLPattern' : 'XPath',
-        implementation = self.document.implementation,
+        implementation = self.document && self.document.implementation,
         MIMEType       = documentMIMEType(),
         doc            = null,
         parser, input, i;
@@ -496,7 +496,9 @@
         }
       }
     } else if (list) {
-      fields = list.split('\r\n');
+      // https://tools.ietf.org/html/rfc7230#page-26
+      // https://tools.ietf.org/html/rfc7230#page-35
+      fields = list.replace(/\r?\n[\t ]+/g, ' ').split('\r?\n');
       len    = fields.length;
       for (i = 0; i < len; ++i) {
         field = fields[i];

commit ded9a19ecd26415947c8f23c4b2f9e7f40822aa0

fix(XHR): typo introduced in 7e56833

index.js
@@ -480,7 +480,7 @@
           fields, field, len, index, name, value, i;
 
       // https://bugzilla.mozilla.org/show_bug.cgi?id=608735
-      if (options.mode === 'cors' && !list) {
+      if (options.mode === 'cors' && list === null) {
         // https://www.w3.org/TR/cors/#simple-response-header
         headers['Cache-Control']    = xhr.getResponseHeader('Cache-Control');
         headers['Content-Language'] = xhr.getResponseHeader('Content-Language');
@@ -503,8 +503,8 @@
       } else if (list) {
         // https://tools.ietf.org/html/rfc7230#page-26
         // https://tools.ietf.org/html/rfc7230#page-35
-        fields = list.replace(/\r?\n[\t ]+/g, ' ').split('\r?\n');
-        len    = fields.length;
+        fields = list.replace(/\r?\n[\t ]+/g, ' ').split(/\r?\n/);
+        len    = fields.length - 1;
         for (i = 0; i < len; ++i) {
           field = fields[i];
           index = field.indexOf(': ');